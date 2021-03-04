15b8919fd5e3e7dc712ec6a63d845a5d2aeace72
40923107 <71478726+40923107@users.noreply.github.com>
2021-03-04 10:46:56 +0800

    Initial commit

---
+++ content/tipuesearch_content.js
@@ -0,0 +1,1 @@
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'This is  https://github.com/mdecourse/cmstemplate 目前的 cmsimde 在編輯器下方新增一個 csave 按鈕, 意即 collabrative save, 當動態網際管理系統在多人同時維護登入維護網頁內容時, 編輯各頁面時段, 該頁面可能已經改版, 因此使用 csave 按鈕存檔時, 會導入當下最新的該頁面內容, 並試圖與編輯中的頁面內容進行合併. 使用 csave 按鈕存檔無法刪除頁面資料. 使用 Edit All 模式, 無法使用 csave 按鈕（尚未測試).', 'tags': '', 'url': 'About.html'}]};
