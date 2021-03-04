4a58abe426d36f7a9a3bea972b136220827bd667
40923107 <40923107@gm.nfu.edu.tw>
2021-03-04 15:23:57 +0800

    initial add

--- init.py
+++ init.py
@@ -11,8 +11,8 @@ class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
     site_title = "CMSiMDE"
-    #ip = "127.0.0.1"
-    ip = "::"
+    ip = "127.0.0.1"
+    #ip = "::"
     port = 9443
     def __init__(self):
         # hope to create downloads and images directories　
